cbeec00548c27d3cfe27b75fc38e125c35f7e192
3203317 <3203317@qq.com>
2017-03-20 20:01:26 +0800

    haha

--- tests/im/im.js
+++ tests/im/im.js
@@ -9,11 +9,12 @@ var console = console || { log: function(){ return; } };
 require.config(config);
 
 
-var fn = function($, _){
+var fn = function($, actor, _){
   console.log('----');
   console.log($)
   console.log(_.min([10, 5, 100, 2, 1000]))
+  console.log(actor)
   console.log('----');
 };
 
-require(['jquery', 'underscore'], fn);
+require(['jquery', 'actor', 'underscore'], fn);
